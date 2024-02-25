3ae8d213db082e051485160f27bb57e0dd17fbe6
saurabh.yadav <saurabh.yadav@deeptek.ai>
2024-02-26 01:27:40 +0530

    Some Changes

---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Task
+
+# Create your views here.
+
+def home(request):
+    context = {'success': False}
+    if request.method == "POST":
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title, desc)
+        ins = Task(taskTitle = title, taskDesc = desc)
+        ins.save()
+        context = {'success' : True}
+    return render(request, 'index.html', context)
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
